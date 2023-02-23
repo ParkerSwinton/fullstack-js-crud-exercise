6fe87d348d126f5dd747649715ed73fc99b66a7e
Parker Swinton <parkerdswinton@gmail.com>
2023-02-23 11:08:49 -0800

    Refactor App to a function component and reorganize styles

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
-import React from 'react';
+import React from 'react'
+import { useState, useEffect } from 'react'
 
-class App extends React.Component {
-  state = {
-    employees: []
-  }
-  
-  componentWillMount = () => {
-    fetch('http://localhost:8080/api/employees')
-      .then(response => response.json())
-      .then(employees => this.setState({ employees }))
-  }
+const App = () => {
+  const [employees, setEmployees] = useState([])
 
-  render() {
-    const {
-      employees
-    } = this.state;
-
-    console.log(this.state);
-
-    return (
-      <div className="App">
-        <h1>Plexxis Employees</h1>
-        {
-          employees.map(employee => (
-            <div key={employee.id}>
-              {
-                Object.keys(employee).map(key => 
-                  <span key={key}>
-                    { key }:
-                    { employee[key] } 
-                  </span>
-                )
-              }
-            </div>
-          ))
-        }
-      </div>
-    );
-  }
+  useEffect(() => {
+    const request = async () => {
+      const apiUrl = 'http://localhost:8080/api/employees'
+      const res = await fetch(apiUrl)
+      const data = await res.json()
+      console.log(data)
+      setEmployees(data)
+    }
+    request()
+  }, [])
+  return (
+    <div className='App'>
+      <h1>Plexxis Employees</h1>
+      {employees.map((employee) => (
+        <div key={employee.id}>
+          {Object.keys(employee).map((key) => (
+            <span key={key}>
+              {key}:{employee[key]}
+            </span>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
 }
-
-export default App;
+export default App
